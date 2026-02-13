c31b1ecf63f4468a4df017902750a9d4e6a4af45
Liam17 <lkg.marengere@gmail.com>
2026-02-13 17:07:13 -0500

    p1

---
+++ board_games.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+grid = np.zeros(shape=(100,100))
+print(grid)
+
+netX = tuple(0,0)
+netY = tuple(100,100)
+ball = tuple(50,50)
+
+x1 = 0
+x2 = 0
+
+y1 = 100
+y2 = 100
+
+## start a game , why can't I just take a nap god? and have a shower and a coffee too? okay, grazie mille
